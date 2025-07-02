989a10ca7b7b155c11c18ac9aa984360236de507
imfejm <4imfejm@gmail.com>
2025-07-02 15:58:34 +0200

    sbox edit click

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ tlacitko.addEventListener("click", () => {
   const rozbal = document.querySelector("#menu");
   rozbal.classList.toggle("hidden");
 });
+
+
+
+
+//otočení karty služeb
+ 
+
+  document.querySelectorAll(".sbox").forEach((box) => {
+    const inner = box.querySelector(".sbox-inner");
+
+    box.addEventListener("click", () => {
+      const isFlipped = box.classList.contains("flip");
+
+      // reset výšky
+      box.style.height = box.offsetHeight + "px";
+
+      // přepnutí třídy
+      box.classList.toggle("flip");
+
+      // po přepnutí spočítáme novou výšku
+      requestAnimationFrame(() => {
+        const newHeight = inner.scrollHeight;
+        box.style.height = newHeight + "px";
+      });
+    });
+  });
